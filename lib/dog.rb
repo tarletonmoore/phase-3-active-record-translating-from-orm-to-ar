9c6c6bc3440749ee7db1de752623e6798bbcb2ff
John Tarleton Moore <tarletonmoore@gmail.com>
2022-09-21 12:56:49 -0400

    commit tree 8811b4efd61156c7f02dbb07e4fe45a27908d3f3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,29 @@
-class Dog
+class Dog < ActiveRecord::Base
+#     attr_accessor :name, :breed
+
+# def initialize(name:, breed:)
+#     @name = name
+#     @breed = breed
+# end
+
+# def self.create_table
+#     sql = <<-SQL
+#           CREATE TABLE IF NOT EXISTS dogs (
+#             id INTEGER PRIMARY KEY,
+#             name TEXT,
+#             breed TEXT
+#           )
+#         SQL
+    
+#         ActiveRecord::Base.connection.execute(sql)
+#         end
+
+# def self.create(hash)
+#   dog =  Dog.create(name:'Ralph', breed:'Lab')
+#   Dog.save
+# dog = Dog.new(hash[:name], hash[:breed])
+# # dog.name = hash[:name]
+# # dog.breed = hash[:breed]
+# dog.save
+# end
 end
